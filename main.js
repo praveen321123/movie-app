18b1684b3dd17a52d7d92c36ee38ace7ae536162
Praveen S <praveens321123@gmail.com>
2024-05-11 13:47:24 +0530

    MongoDB connected

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import Express from "express";
 import MoviesRoute from "./routes/movies-routes.js"
+import connectDb from "./lib/db.js";
 
 const app = Express()
 const port = 3000
+
+connectDb()
 
 app.get('/', (req, res)=> {
     res.json({msg: "Hello world"})
@@ -11,5 +14,5 @@ app.get('/', (req, res)=> {
 app.use("/movies", MoviesRoute)
 
 app.listen(port, () => {
-    console.log(`Running in http://localhost:${port}`)
+    console.log(`Running in http://localhost:${port}`) 
 })
